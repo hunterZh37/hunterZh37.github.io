dbc68a502327657aeb755c471df6a20871e00f14
hunterZh37 <71850140+hunterZh37@users.noreply.github.com>
2024-09-10 14:54:29 -0700

    brand new template

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,6 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-
-import react from "@astrojs/react";
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://hunterZh37.github.io',
-  integrations: [tailwind(), react()]
+    site: 'https://hunterzh37.github.io',
 });
